e9dc9df5066db9233052e97c8d6f8b77cfb85d7d
rekarromar <rekarromar@gmail.com>
2022-12-12 21:46:52 +0300

    generate Image

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
-
+var cors = require('cors')
 const bodyParser = require('body-parser')
 
 const app = express();
+
+app.use(cors()) 
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
--- controllers/imageControllers.js
+++ controllers/imageControllers.js
@@ -1,45 +1,32 @@
-const { translate } = require('bing-translate-api');
-
-
 
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
+
 const openai = new OpenAIApi(configuration);
 
 const generateImage = async (req, res) => {
 
         try {
-
-            const word = await translate(req.body.prompt, 'ku', 'en', true);
-
-            console.log(word.translation);
-
             const response = await openai.createImage({
-                prompt: word.translation,
+                prompt: req.body.prompt,
                 n: 1,
                 size: "1024x1024"
             })
     
-            console.log(req.body);
-    
             const imageUrl = response.data.data[0].url;
     
             res.status(200).json({
-                url: imageUrl,
+                image: imageUrl,
             })
         } catch (error) {
-            res.status(400).json({
-                msg: 'asdfasd',
+            res.json({
+                Error: 'Ooops..',
             })
         }
- 
-
-    
-
-    
+   
 }
 
 module.exports = {generateImage}
